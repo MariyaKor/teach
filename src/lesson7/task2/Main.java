ba42bac429fd89b4ecfbb7ce4852983922f55edc
MariyaKor <korotmarie@gmail.com>
2022-10-16 20:58:06 +0300

    the seventh lesson task two commit

--- src/lesson7/task2/Main.java
+++ src/lesson7/task2/Main.java
@@ -1,13 +1,11 @@
 package lesson7.task2;
 
-public class Main {
-
 /**
  * 2. Создать классы "Директор", "Рабочий", "Бухгалтер".
  * Реализовать интерфейс с методом, который печатает название
  * должности и имплементировать этот метод в созданные классы.
  */
-
+public class Main {
     public static void main (String[] args) {
         Worker perfectWorker = new Worker();
         Accountant perfectAccountant = new Accountant();
